338fe268c3b38f7dec6b6238679aca14c21d6517
Jordi Hereu <jhereumayo@gmail.com>
2021-02-07 13:38:00 +0100

    Rounds and milliseconds interval as arguments and color output for player scores

--- engine/game.py
+++ engine/game.py
@@ -3,9 +3,12 @@
 import sys, time
 import engine, botplayer
 import view
+import time
 
 cfg_file = sys.argv[1]
-bots = sys.argv[2:]
+rounds = int(sys.argv[2])
+interval = int(sys.argv[3])
+bots = sys.argv[4:]
 DEBUG = False
 CONTINUE_ON_ERROR = False
 
@@ -18,10 +21,26 @@ for actor in actors:
 
 view = view.GameView(game)
 
+# Player color codes (same as view.py)
+# 256 color ANSI format: \u001b[48;5;${i}m
+colors = [
+    1, #(255, 0, 0), red
+    4, #(0, 0, 255), blue
+    2, #(0, 255, 0), green
+    3, #(255, 255, 0), yellow
+    6, #(0, 255, 255), cyan
+    13, #(255, 0, 255), magenta
+    208, #(255, 127, 0), orange
+    210 #(255, 127, 127), salmon
+]
+color_reset = "\u001b[0m"
+color_bold = "\033[1m"
+
 round = 0
-while True:
+while round < rounds:
     game.pre_round()
     view.update()
+
     for actor in actors:
         try:
             actor.turn()
@@ -33,10 +52,15 @@ while True:
                 actor.close()
         view.update()
     game.post_round()
-    s = "########### ROUND %d SCORE: " % round
+
+    s = "%s%sROUND %d / %d\n\n" % (color_reset, color_bold, round, rounds)
+
     for i in range(len(bots)):
-        s += "P%d: %d " % (i, game.players[i].score)
+        color_player = "\u001b[38;5;%dm" % colors[i]
+        s += "%s%sP%d (%s):\t %d \n" % (color_reset, color_player, i, game.players[i].name, game.players[i].score)
     print(s)
+
     round += 1
+    time.sleep(interval / 1000)
 
 view.update()
